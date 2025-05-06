8bee7c40aa6bcef29f27dbd47b3237eb36f2edb8
DiegoR-06 <diego.ruano@cesjuanpablosegundo.es>
2025-05-06 21:44:01 +0200

    ESlint, login added. Login not working for the moment

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -1,21 +1,43 @@
-import { dbClient, testConnection} from './db.mjs'
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import { testConnection } from './db.mjs'
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 3000;
+dotenv.config()
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const app = express()
+const PORT = process.env.PORT || 3000
 
-// Middleware
-app.use(cors());              // Habilita CORS para todas las rutas
-app.use(express.json());      // Parsear JSON
+app.use(cors())
+app.use(express.json())
 
-// Rutas
-testConnection();
+testConnection()
 
-// Iniciar el servidor
+// Ruta de login (sin aplicar checkAuth)
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.join(dirname, '../frontend/login.html'))
+})
+
+app.use(express.static(path.join(dirname, '../frontend')))
+
+// Ruta principal (sin restricción)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(dirname, '../frontend/index.html'))
+})
+
+// Ruta para manejar el login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body
+  if (username === 'diego' && password === '1234') {
+    return res.json({ message: 'Login exitoso' })
+  } else {
+    return res.status(401).json({ message: 'Credenciales incorrectas' })
+  }
+})
+
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
-
+  console.log(`Servidor escuchando en http://localhost:${PORT}`)
+})
