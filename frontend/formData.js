015dbbd7960f8a9b3821c33a985da2e32dbe492d
DiegoR-06 <diego.ruano@cesjuanpablosegundo.es>
2025-05-06 21:45:48 +0200

    Same as committed before, was in wrong dir

---
+++ frontend/formData.js
@@ -0,0 +1,16 @@
+// Carga datos del formulario para que los use el backend
+const form = document.querySelector("form")
+form.addEventListener('submit', async (event) => {
+	event.preventDefault();
+	const data = new FormData(form);
+	const response = await fetch('http://localhost:3000/login', {
+		method: 'POST',
+		body: JSON.stringify({
+			username: data.get('username'),
+			password: data.get('password'),
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+})
--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,4 @@
 // FECHA POR DEFECTO LA DE HOY
 const hoy = new Date().toISOString().split("T")[0]; // formato YYYY-MM-DD
 document.getElementById("fecha").value = hoy;
+
